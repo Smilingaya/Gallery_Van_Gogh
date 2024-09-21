fff732e5cdd2b1bfe625063156b7a70a9d4b7adf
Smilingaya <alizeroukiaya0911@gmail.com>
2024-09-22 00:45:44 +0200

    van gogh

--- server/app.js
+++ server/app.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const app = express();
 const blogRoutes = require("./routes/BlogRoutrs");
 app.use(express.json());
+const PORT = 4000;
 //connect to mongodb
 const dbUrl =
   "mongodb+srv://aya:12345@smiling.pgesm.mongodb.net/Blog_img?retryWrites=true&w=majority";
 mongoose
   .connect(dbUrl)
-  .then((resault) => app.listen(4000))
+  .then((resault) => app.listen(PORT))
   .catch((err) => console.log(err));
 
 app.use("/api/blogs", blogRoutes);
